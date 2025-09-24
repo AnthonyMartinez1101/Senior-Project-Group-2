9e90f500ced80210065a2fbdd447b2ae65f6dd88
AdrianSaysHi <adrian@delibassis.com>
2025-09-24 01:13:04 -0700

    Added background and interactable tilemaps in FarmTest

--- Senior Project/Assets/Scripts/TileManager.cs
+++ Senior Project/Assets/Scripts/TileManager.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
-public class TileManager
-{
-   [SerializeField] private Tilemap interactableTile; // Tilemap to manage
-   [SerializeField] private Tile hiddenTile; // Tile to set
 
-   void Start()
-   {
-      foreach(var position in interactableTile.cellBounds.allPositionsWithin) // Iterate through all positions in the tilemap's bounds
-      {
-        interactableTile.SetTile(position, hiddenTile); // Set the tile at the current position to the hidden tile
-      }
-   }
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap interactableMap; // Tilemap to manage
+    [SerializeField] private Tile hiddenTile; // Tile to set
+
+    void Start()
+    {
+        foreach(var position in interactableMap.cellBounds.allPositionsWithin) // Iterate through all positions in the tilemap's bounds
+        {
+            interactableMap.SetTile(position, hiddenTile); // Set the tile at the current position to the hidden tile
+        }
+    }
 }
