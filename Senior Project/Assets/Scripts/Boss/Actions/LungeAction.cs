5eeeb1dc8c28ae48ca679d63d9610d9256e8b7b3
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2026-02-06 01:45:33 -0800

    boss scene1 has the ant prototype

--- Senior Project/Assets/Scripts/Boss/Actions/LungeAction.cs
+++ Senior Project/Assets/Scripts/Boss/Actions/LungeAction.cs
@@ -4,23 +4,69 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Lunge", menuName = "Boss/Actions/Lunge")]
 public class Lunge : BossAction
 {
-    //public float speed = 10f;
+    public float speed = 10f;
+    public float backSpeed = 5f;
+    public float backDistance = 5f;
+    private bool isLunging = false;
+    public float ptwoRage = 1.5f;
+    public float backDivider = 1.5f;
+
     public override void ExecuteAction(BossScript boss)
     {
-        boss.StartCoroutine(ShortJump(boss));
+        if (isLunging) return;
+        isLunging = true;
+        boss.StartCoroutine(ShortJump(boss));  
+        
     }
 
     private IEnumerator ShortJump(BossScript boss)
     {
-        //stop the follow script? 
         boss.agent.isStopped = true; 
-        float elapsed = 0f; 
-        while (elapsed < actionDuration)
+
+        yield return boss.StartCoroutine(Back(boss));
+        yield return boss.StartCoroutine(Forward(boss));
+        
+        boss.agent.isStopped = false; 
+    }
+
+    private IEnumerator Back(BossScript boss)
+    {
+       Vector2 startPosition = boss.transform.position;
+       Vector2 player = boss.player.position;
+       Vector2 direction = (startPosition - player).normalized;
+       float backMod = backDistance;
+       if(boss.phaseTwoActivated)
+       {
+        backMod = backDistance / Mathf.Max(0.0001f, ptwoRage);
+       }
+       Vector2 backTartget = startPosition + direction * backMod;
+
+       while(Vector2.Distance((Vector2)boss.transform.position, backTartget) > 0.05f)
+       {
+            Vector2 newPos = Vector2.MoveTowards(boss.transform.position, backTartget, backSpeed * Time.deltaTime);
+            boss.transform.position = new Vector3(newPos.x, newPos.y, boss.transform.position.z);
+            yield return null;
+       }
+
+    }
+
+    private IEnumerator Forward(BossScript boss)
+    {
+        Vector2 lungeTarget = boss.player.position;
+        float forwardMod = speed;
+
+        if(boss.phaseTwoActivated)
         {
-            //agent.Move(um * speed * Time.deltaTime)
-            elapsed += Time.deltaTime;
+            forwardMod = speed * ptwoRage;
+        }
+
+        float time = 0f;
+        while(time < actionDuration)
+        {
+            time += Time.deltaTime;
+            Vector2 newPos = Vector2.MoveTowards(boss.transform.position, lungeTarget, forwardMod * Time.deltaTime);
+            boss.transform.position = new Vector3(newPos.x, newPos.y, boss.transform.position.z);
             yield return null;
         }
-        boss.agent.isStopped = false; 
     }
 }
