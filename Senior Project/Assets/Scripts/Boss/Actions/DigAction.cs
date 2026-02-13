e2b4fd9d2c4f1b8a7b405c0673d89e4c4ae398c6
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2026-02-12 16:12:08 -0800

    working on dig

--- Senior Project/Assets/Scripts/Boss/Actions/DigAction.cs
+++ Senior Project/Assets/Scripts/Boss/Actions/DigAction.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dig", menuName = "Boss/Actions/Dig")]
 public class Dig : BossAction
 {
+    public GameObject warning;
+    public float disappearDuration = 1f;
+    public float flashDuration = 0.8f;
+    public int flashCount = 3;
+    public float teleportDistanceMin = 1.0f;
+    public float teleportDistanceMax = 2.5f;
 
     public override void ExecuteAction(BossScript boss)
     {
@@ -12,6 +18,29 @@ public class Dig : BossAction
 
     private IEnumerator DigCoroutine(BossScript boss)
     {
-        yield return null;
+        Vector2 playerStartPos = boss.GetPlayerPosition();
+        float disappearTime = disappearDuration;
+        Vector3 originalPos = boss.transform.position;
+
+        //Spawn warning at player's position
+        GameObject warningInstance = null;
+        if(warning != null)
+        {
+            warningInstance = Object.Instantiate(warning, originalPos, Quaternion.identity);
+        }
+
+        SpriteRenderer[] renderers = warning.GetComponentsInChildren<SpriteRenderer>();
+        Collider[] colliders = warning.GetComponentsInChildren<Collider>();
+
+        var agent = boss.agent;
+        if(agent != null )
+        {
+          agent.enabled = false; 
+        }
+
+        foreach (var r in renderers) r.enabled = false; //renderers are disabled immediately to prevent flashing of the boss before it disappears
+        foreach (var c in renderers) c.enabled = false; //colliders are disabled immediately to prevent the boss from being hit while disappearing
+
+        yield return null; //delete when code is done
     }
 }
