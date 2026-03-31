086238affda28f4d6d8773f9242806c851d7fadf
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2026-03-31 00:02:37 -0700

    Added sprinkler sprites

---
+++ Senior Project/Assets/Scripts/Boss/Actions/WebAction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Web", menuName = "Boss/Actions/Web")]
+
+public class WebAction : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
