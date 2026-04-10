a3dafdfcbb0fd7108318c44b23c7a24f71ccab45
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2026-04-10 00:13:59 -0700

    Environment Change script

---
+++ Senior Project/Assets/Scripts/EnviornmentChange.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class EnviornmentChange : MonoBehaviour
+{
+    public WorldClock time;
+
+    public Sprite Spring;
+    public Sprite Summer;
+    public Sprite Fall;
+    public Sprite Winter;
+
+    public GameObject background;
+
+    void Start()
+    {
+        UpdateSeasonalDecor();
+    }
+
+    void Update()
+    {
+        UpdateSeasonalDecor();
+    }
+    public void UpdateSeasonalDecor()
+    {
+        switch (time.CurrentSeason)
+        {
+            case SeasonPhase.Spring:
+                background.GetComponent<SpriteRenderer>().sprite = Spring;
+                break;
+            case SeasonPhase.Summer:
+                background.GetComponent<SpriteRenderer>().sprite = Summer;
+                break;
+            case SeasonPhase.Fall:
+                background.GetComponent<SpriteRenderer>().sprite = Fall;
+                break;
+            case SeasonPhase.Winter:
+                background.GetComponent<SpriteRenderer>().sprite = Winter;
+                break;
+        }
+    }
+}
