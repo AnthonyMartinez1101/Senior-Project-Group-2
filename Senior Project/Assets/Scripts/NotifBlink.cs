69aee74548e4708b3666f510aa7e292e6a6060ad
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2026-04-23 15:09:52 -0700

    notif blinks

---
+++ Senior Project/Assets/Scripts/NotifBlink.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotifBlink : MonoBehaviour
+{
+    [SerializeField] private Image img;
+    [SerializeField] private Sprite notif;
+    [SerializeField] private float interval = 0.2f;
+
+    private Coroutine blinkCoroutine;
+    private bool isBlinking = false;
+    private void Awake()
+    {
+        if (img == null) img = GetComponent<Image>();
+    }
+
+    public void EnableNotif()
+    {
+        if(img == null) return;
+        if(notif!= null) 
+        {
+            img.sprite = notif;
+        }
+        if (isBlinking) return;
+
+        isBlinking = true;
+        blinkCoroutine = StartCoroutine(Blink());
+    }
+
+    public void DisableNotif()
+    {
+        if (img == null) return;
+        isBlinking= false;
+        if (blinkCoroutine != null)
+        {
+            StopCoroutine(blinkCoroutine);
+            blinkCoroutine = null;
+        }
+        img.enabled = false;
+    }
+
+    private IEnumerator Blink()
+    {
+        while (isBlinking)
+        {
+            img.enabled = !img.enabled;
+            yield return new WaitForSeconds(interval);
+            img.enabled = img.enabled;
+            yield return new WaitForSeconds(0.4f);
+            img.enabled = !img.enabled;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
