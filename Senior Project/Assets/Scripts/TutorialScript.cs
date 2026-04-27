46d5444af800414edc2bcc28be2d2dfd43ecdac4
AdrianSaysHi <adrian@delibassis.com>
2026-04-27 01:27:41 -0700

    Final Demo Commit

--- Senior Project/Assets/Scripts/TutorialScript.cs
+++ Senior Project/Assets/Scripts/TutorialScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour
@@ -32,12 +33,19 @@ public class TutorialScript : MonoBehaviour
 
     public GameObject tutoritalEnemy;
 
+    InputAction toggleInput;
+    InputAction rightStick;
+    private bool controllerConnected = false;
+
     private void Start()
     {
         WASD = InputSystem.actions.FindAction("Move");
         Space = InputSystem.actions.FindAction("Dash");
 
         scythe = InputSystem.actions.FindAction("Scythe");
+
+        toggleInput = InputSystem.actions.FindAction("ToggleInput");
+        rightStick = InputSystem.actions.FindAction("Look");
 
         // Lock mechanics here
         player.GetComponent<InteractScript>().canPlant = false;
@@ -48,7 +56,7 @@ public class TutorialScript : MonoBehaviour
         CanCollectItems(false);
         shop.interactable = false;
 
-        ui.UpdateUI("Use WASD to move around", false);
+        ui.UpdateUI("Use WASD or left stick to move around", false);
 
         StartCoroutine(TutorialSequence());
     }
@@ -80,6 +88,9 @@ public class TutorialScript : MonoBehaviour
         yield return StartCoroutine(WaterCropsStage());
         Debug.Log("Water crops complete");
 
+        yield return StartCoroutine(DefeatDayEnemies());
+        Debug.Log("Defeat day enemies complete");
+
         yield return StartCoroutine(HarvestCropsStage());
         Debug.Log("Harvest crops complete");
 
@@ -91,6 +102,9 @@ public class TutorialScript : MonoBehaviour
 
         yield return StartCoroutine(SellItemsStage());
         Debug.Log("Sell items complete");
+
+        yield return StartCoroutine(BuyItemsStage());
+        Debug.Log("Buy items complete");
 
         ui.UpdateUI("Tutorial Complete!", true);
         Debug.Log("Tutorial complete, continuing to main game");
@@ -119,7 +133,8 @@ public class TutorialScript : MonoBehaviour
 
     private IEnumerator DashStage()
     {
-        ui.UpdateUI("Use Space to dash while moving", false);
+        if (controllerConnected) ui.UpdateUI("Use right face button to dash while moving", false);
+        else ui.UpdateUI("Use Space to dash while moving", false);
 
         bool dash = false;
         while (!dash)
@@ -137,7 +152,8 @@ public class TutorialScript : MonoBehaviour
     //Wait for scythe to be used
     private IEnumerator ScytheStage()
     {
-        ui.UpdateUI("Right click to use Scythe", false);
+        if (controllerConnected) ui.UpdateUI("Press left trigger to use Scythe", false);
+        else ui.UpdateUI("Right click to use Scythe", false);
 
         bool scytheUsed = false;
         while (!scytheUsed)
@@ -199,7 +215,8 @@ public class TutorialScript : MonoBehaviour
     // Progress when seeds leave inventory
     private IEnumerator PlantCropsStage()
     {
-        ui.UpdateUI("Plant the seeds", false);
+        if (controllerConnected) ui.UpdateUI("Press right trigger or bottom face button on a highlighted soil plot to plant seeds", false);
+        else ui.UpdateUI("Plant the seeds by interacting with a highlighted soil plot", false);
 
         bool cropsPlanted = false;
         player.GetComponent<InteractScript>().canPlant = true;
@@ -217,7 +234,8 @@ public class TutorialScript : MonoBehaviour
     // Progress when water bucket is filled or when well is interacted with
     private IEnumerator FillBucketStage()
     {
-        ui.UpdateUI("Interact with the well to fill your bucket", false);
+        if (controllerConnected) ui.UpdateUI("Press right trigger or bottom face button near the well to fill your bucket", false);
+        else ui.UpdateUI("Interact with the well to fill your bucket", false);
 
         bool wellInteraction = false;
         player.GetComponent<InteractScript>().canWater = true;
@@ -235,7 +253,8 @@ public class TutorialScript : MonoBehaviour
     // Progress when all crops are watered
     private IEnumerator WaterCropsStage()
     {
-        ui.UpdateUI("Water your planted crops", false);
+        if (controllerConnected) ui.UpdateUI("Using your bucket, press right trigger or bottom face button near your crops to water them", false);
+        else ui.UpdateUI("Use your bucket to interact and water the planted crops", false);
 
         bool cropsWatered = false;
         while (!cropsWatered)
@@ -249,10 +268,25 @@ public class TutorialScript : MonoBehaviour
         yield return StartCoroutine(SetCheck());
     }
 
+    private IEnumerator DefeatDayEnemies()
+    {
+        ui.UpdateUI("Bugs are eating your crops! Defeat them with your scythe!", false);
+        bool enemiesDefeated = false;
+        while (!enemiesDefeated)
+        {
+            if (EnemyCollection.childCount == 0)
+            {
+                enemiesDefeated = true;
+            }
+            yield return null;
+        }
+        yield return StartCoroutine(SetCheck());
+    }
+
     // Progress when all crops are killed/harvested
     private IEnumerator HarvestCropsStage()
     {
-        ui.UpdateUI("Harvest your fully grown crops with your scythe", false);
+        ui.UpdateUI("Harvest your fully grown crops with your scythe by attacking them", false);
 
         bool cropsHarvested = false;
         while (!cropsHarvested)
@@ -288,7 +322,7 @@ public class TutorialScript : MonoBehaviour
     // Progress when food is eaten from inventory (would like to rewrite later)
     private IEnumerator EatFoodStage()
     {
-        ui.UpdateUI("Eat food to regain health", false);
+        ui.UpdateUI("Interact and eat the harvested produce to regain health", false);
 
         player.GetComponent<InteractScript>().canEat = true;
 
@@ -299,6 +333,7 @@ public class TutorialScript : MonoBehaviour
             {
                 player.GetComponent<PlayerHealth>().SetMaxHealth();
                 foodEaten = true;
+                player.GetComponent<InteractScript>().canEat = true;
             }
             yield return null;
         }
@@ -308,7 +343,8 @@ public class TutorialScript : MonoBehaviour
     // Progress when all produce is sold
     private IEnumerator SellItemsStage()
     {
-        ui.UpdateUI("Go to the shop, press E, and sell your harvested crops", false);
+        if (controllerConnected) ui.UpdateUI("Press top face button near the shop to open it and sell your products", false);
+        else ui.UpdateUI("Go to the shop and open it with E, then sell your products", false);
 
         shop.interactable = true;
         while (!inv.HasSold)
@@ -317,6 +353,34 @@ public class TutorialScript : MonoBehaviour
         }
         yield return StartCoroutine(SetCheck());
     }
+
+
+    private IEnumerator BuyItemsStage()
+    {
+        ui.UpdateUI("Buy some items you can afford, and open your package with your trusty scythe!", false);
+
+        bool hasOpenedPackage = false;
+        bool foundPackage = false;
+        Package package = Object.FindFirstObjectByType<Package>();
+        while (!hasOpenedPackage)
+        {
+            if (!package && !foundPackage)
+            {
+                package = Object.FindFirstObjectByType<Package>();
+            }
+            if (package)
+            {
+                foundPackage = true;
+            }
+            if(foundPackage && !package)
+            {
+                hasOpenedPackage = true;
+            }
+            yield return null;
+        }
+        yield return StartCoroutine(SetCheck());
+    }
+
 
     private void CanCollectItems(bool flag)
     {
@@ -330,6 +394,54 @@ public class TutorialScript : MonoBehaviour
     private IEnumerator SetCheck()
     {
         ui.Checkmark(true);
-        yield return new WaitForSeconds(2f);
+        // run scaninputtype for a few seconds to allow player to switch input types if they want
+        float timer = 2f;
+        while (timer > 0f)
+        {
+            ScanInputType();
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+    
+    private void ScanInputType()
+    {
+        // Keyboard & mouse input
+        if (controllerConnected)
+        {
+            if (Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                controllerConnected = false;
+            }
+            if (Mouse.current != null && (
+                Mouse.current.leftButton.wasPressedThisFrame ||
+                Mouse.current.rightButton.wasPressedThisFrame ||
+                Mouse.current.middleButton.wasPressedThisFrame ||
+                Mouse.current.delta.ReadValue() != Vector2.zero))
+            {
+                controllerConnected = false;
+            }
+        }
+
+        // Gamepad input
+        else
+        {
+            if (Gamepad.current != null)
+            {
+                foreach (var control in Gamepad.current.allControls)
+                {
+                    if (control is ButtonControl btn && btn.wasPressedThisFrame)
+                    {
+                        controllerConnected = true;
+                    }
+                }
+                if (Gamepad.current.leftStick.ReadValue() != Vector2.zero ||
+                    Gamepad.current.rightStick.ReadValue() != Vector2.zero)
+                {
+                    controllerConnected = true;
+                }
+            }
+        }
     }
 }
